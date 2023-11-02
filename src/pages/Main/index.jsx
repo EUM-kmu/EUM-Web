688a561024184c53c4ca7057c11ab5c8c40acacc
Jiwon Choi <jwchoi@lablup.com>
2023-11-02 18:00:44 +0900

    feat: `NewsButton` 필요 icon 파일 추가, List 형태로 버튼 관리
    - `NewsButton` 컴포넌트에 필요한 imageUrl 관련 icon 추가
    - `map`을 통하여 관리하기 위하여 list 형태로 버튼 관리

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,4 +1,16 @@
+import NewsButton from "../../components/NewsButton";
+import centerIcon from "../../images/center_icon.png";
+import noticeIcon from "../../images/notice_icon.png";
+import newsIcon from "../../images/news_icon.png";
+
+
 const MainPage = () => {
+    const btnType = [
+        {text: "동네 소식", imageUrl: newsIcon},
+        {text: "자치회관", imageUrl: centerIcon},
+        {text: "공고", imageUrl: noticeIcon},
+    ]
+
     return (
         <><p>MainPage</p></>
     )
