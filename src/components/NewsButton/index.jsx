d92a58d2b95f3099118f063dfd0cbfcbc42fb232
Jiwon Choi <jwchoi@lablup.com>
2023-11-12 19:20:36 +0900

    feat: `NewsButton` Component UI 구현
    - '동네 소식', '자치 회관', '공고' 버튼에 사용되는 버튼을 재사용 가능 Component로 구현
    - iconSrc, text, onClickEvent를 파라미터에 추가

---
+++ src/components/NewsButton/index.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { ButtonContainer, Icon, Text } from "./styles"
+
+const NewsButton = ({iconSrc, text, onClickEvent}) => {
+    return (
+        <ButtonContainer
+            onClick={ onClickEvent }
+        >
+            <Icon src={iconSrc} />
+            <Text>{text}</Text>
+        </ButtonContainer>
+    )
+}
+
+export default NewsButton;
