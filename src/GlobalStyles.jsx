81cbbd608d4c61cb9fa5597351f02737cdff7995
Jiwon Choi <jwchoi@lablup.com>
2023-11-16 01:19:41 +0900

    fix: Pretendard 폰트 미적용 수정

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,65 +1,75 @@
 import { createGlobalStyle } from "styled-components";
 
+import PretendardThin from "./assets/fonts/Pretendard-Thin.woff";
+import PretendardExtraLight from "./assets/fonts/Pretendard-ExtraLight.woff";
+import PretendardLight from "./assets/fonts/Pretendard-Light.woff";
+import PretendardRegular from "./assets/fonts/Pretendard-Regular.woff";
+import PretendardMedium from "./assets/fonts/Pretendard-Medium.woff";
+import PretendardSemiBold from "./assets/fonts/Pretendard-SemiBold.woff";
+import PretendardBold from "./assets/fonts/Pretendard-Bold.woff";
+import PretendardExtraBold from "./assets/fonts/Pretendard-ExtraBold.woff";
+import PretendardBlack from "./assets/fonts/Pretendard-Black.woff";
+
 const GlobalStyles = createGlobalStyle`
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-Thin.woff');
+        src: url(${PretendardThin}) format('woff');
         font-weight: 100;
         font-style: thin;
     }
 
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-ExtraLight.woff');
+        src: url(${PretendardExtraLight}) format('woff');
         font-weight: 200;
         font-style: extra-light;
     }
 
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-Light.woff');
+        src: url(${PretendardLight}) format('woff');
         font-weight: 300;
         font-style: light;
     }
 
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-Regular.woff');
+        src: url(${PretendardRegular}) format('woff');
         font-weight: 400;
         font-style: normal;
     }
 
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-Medium.woff');
+        src: url(${PretendardMedium}) format('woff');
         font-weight: 500;
         font-style: medium;
     }
 
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-SemiBold.woff');
+        src: url(${PretendardSemiBold}) format('woff');
         font-weight: 600;
         font-style: semi-bold;
     }
 
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-Bold.woff');
+        src: url(${PretendardBold}) format('woff');
         font-weight: 700;
         font-style: bold;
     }
 
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-ExtraBold.woff');
+        src: url(${PretendardExtraBold}) format('woff');
         font-weight: 800;
         font-style: extra-bold;
     }
 
     @font-face {
         font-family: Pretendard;
-        src: url('./assets/fonts/Pretendard-Black.woff');
+        src: url(${PretendardBlack}) format('woff');
         font-weight: 900;
         font-style: black;
     }
