75b96ccafe4e2256404259ef4a4b61925ec68da0
Jiwon Choi <jwchoi@lablup.com>
2023-12-05 17:10:05 +0900

    refactor:  tailwind css를 이용한`PostHeader` 컴포넌트 리팩토링

---
+++ src/components/PostHeader/index.jsx
@@ -0,0 +1,16 @@
+import arrow from "../../assets/right_arrow.svg";
+
+// eslint-disable-next-line react/prop-types
+const PostHeader = ({ title }) => {
+  return (
+    <div className="flex justify-between">
+      <p className="font-bold text-xl">{title}</p>
+      <div className="flex cursor-pointer items-center">
+        <p className="font-medium text-[#8E8E93]">더보기</p>
+        <img src={arrow} />
+      </div>
+    </div>
+  );
+};
+
+export default PostHeader;
