5dfdb87d71b96c364a784b133e54ddca726b5e8f
Jiwon Choi <jwchoi@lablup.com>
2023-12-06 01:21:17 +0900

    fix: 뒤로가기 버튼 인식 범위 수정(아이콘만 해당)

--- src/components/BackButton/index.jsx
+++ src/components/BackButton/index.jsx
@@ -5,16 +5,19 @@ import arrow from "../../assets/left_arrow.svg";
 const BackButton = ({ text }) => {
   const navigate = useNavigate();
   return (
-    <div
-      className="flex gap-[8px] items-center py-2"
-      onClick={() => {
-        navigate(-1); // 뒤로 가기
-      }}
-    >
-      <div className="w-6 h-6 flex justify-center items-center">
-        <img src={arrow} />
+    <div className="sticky top-0">
+      <div className="flex gap-[8px] items-center py-2">
+        <div className="w-6 h-6 flex justify-center items-center">
+          <img
+            className="cursor-pointer"
+            src={arrow}
+            onClick={() => {
+              navigate(-1); // 뒤로 가기
+            }}
+          />
+        </div>
+        <p className="text-xl font-bold">{text}</p>
       </div>
-      <p className="text-xl font-bold">{text}</p>
     </div>
   );
 };
