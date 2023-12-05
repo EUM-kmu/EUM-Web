f0e3284006c680601004f43d9184b6e273da23eb
Jiwon Choi <jwchoi@lablup.com>
2023-12-06 01:23:05 +0900

    feat: `더보기` 버튼 클릭 시 `ListPage`로 redirect

--- src/components/PostHeader/index.jsx
+++ src/components/PostHeader/index.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import arrow from "../../assets/right_arrow.svg";
 
 // eslint-disable-next-line react/prop-types
 const PostHeader = ({ title }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between py-4">
       <p className="font-bold text-xl">{title}</p>
-      <div className="flex cursor-pointer items-center">
+      <div
+        className="flex cursor-pointer items-center"
+        onClick={() => {
+          navigate("/list");
+        }}
+      >
         <p className="font-medium text-[#8E8E93]">더보기</p>
         <img src={arrow} />
       </div>
