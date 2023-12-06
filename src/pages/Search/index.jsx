07ee091847f1f5ef75b8e874e5651312e57ca8df
Jiwon Choi <jwchoi@lablup.com>
2023-12-06 14:41:16 +0900

    feat: 검색 필터링 로직 적용

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -2,6 +2,7 @@ import searchIcon from "../../assets/search_icon.svg";
 import removeIcon from "../../assets/remove_icon.svg";
 import { useState } from "react";
 import BackButton from "../../components/BackButton";
+import seoulDistrictsData from "../../data/seoulDistrictsData.json";
 
 const SearchPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -9,6 +10,39 @@ const SearchPage = () => {
     setSearchTerm(event.target.value);
     console.log(searchTerm);
   };
+
+  const filteredData = Object.entries(seoulDistrictsData).reduce(
+    (acc, [city, districts]) => {
+      if (city.includes(searchTerm)) {
+        acc[city] = districts;
+      } else {
+        const filteredDistricts = Object.entries(districts).reduce(
+          (acc, [district, dongs]) => {
+            if (district.includes(searchTerm)) {
+              acc[district] = dongs;
+            } else {
+              const filteredDongs = dongs.filter((dong) =>
+                dong.includes(searchTerm)
+              );
+              if (filteredDongs.length > 0) {
+                acc[district] = filteredDongs;
+              }
+            }
+            return acc;
+          },
+          {}
+        );
+        if (Object.keys(filteredDistricts).length > 0) {
+          acc[city] = filteredDistricts;
+        }
+      }
+      return acc;
+    },
+    {}
+  );
+
+  console.log(seoulDistrictsData);
+  console.log(filteredData);
 
   return (
     <div className="flex flex-col h-screen">
@@ -39,6 +73,7 @@ const SearchPage = () => {
       ) : (
         <div className="flex-1">
           <p>&rsquo;{searchTerm}&rsquo; 검색 결과</p>
+          <div>검색 결과 보여질 목록</div>
         </div>
       )}
     </div>
